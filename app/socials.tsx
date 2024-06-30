fb05e5b1a90bd7b42505faf9b775f748ebe2a392
Nghiep <me@nghiep.dev>
2024-06-30 20:47:44 +0700

    Add socials

--- app/socials.tsx
+++ app/socials.tsx
@@ -1,3 +1,18 @@
 export default function Socials() {
-  return <div>socials</div>;
+  return (
+    <div className='pt-5 flex gap-x-4 sm:*:multi-[w-[90px];px-2;py-2] *:multi-[aspect-square;w-[65px];px-1;py-1;rounded-2xl;bg-white;flex;items-center;justify-center] [&_img]:multi-[size-full]'>
+      <a href={process.env.NEXT_PUBLIC_TWITTER_URL} target='_x'>
+        <img src='/x.svg' alt='' />
+      </a>
+      <a href={process.env.NEXT_PUBLIC_TELEGRAM_URL} target='_tele'>
+        <img src='/tele.svg' alt='' className='scale-150' />
+      </a>
+      <a href={process.env.NEXT_PUBLIC_DEX_URL} target='_dex'>
+        <img src='/dex.svg' alt='' />
+      </a>
+      <a href={process.env.NEXT_PUBLIC_DEXS_URL} target='_dexs'>
+        <img src='/dexs.svg' alt='' className='scale-150' />
+      </a>
+    </div>
+  );
 }
